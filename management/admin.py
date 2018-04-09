0b23092bfd8682fa36e7f0b4bbf5c0fad47ecc2f
jellyfishOne <jrenteria.camino@gmail.com>
2018-04-08 21:22:21 -0700

    Added PropertyListing model and registered it to the admin site

--- management/admin.py
+++ management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from management.models import State, Country
+from management.models import State, Country, PropertyListing
 
 admin.site.register(State)
 admin.site.register(Country)
+admin.site.register(PropertyListing)
--- management/models.py
+++ management/models.py
@@ -15,3 +15,18 @@ class Country(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class PropertyListing(models.Model):
+	state = models.ForeignKey(State, on_delete=models.CASCADE)
+	address = models.CharField(max_length=128)
+	city = models.CharField(max_length=50)
+	zip_code = models.CharField(max_length=12)
+	beds = models.IntegerField()
+	baths = models.FloatField()
+	available_date = models.DateField(auto_now=False, auto_now_add=False)
+
+	class Meta:
+		verbose_name_plural = 'Property Listings'
+
+	def __str__(self):
+		return '%s %s' % (self.address, self.city)
